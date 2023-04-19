3d798f6e50e765946eaa264ce8cae33023971f71
sumon-hossain <sumonengr.96@gmail.com>
2023-04-19 13:44:23 +0600

    wip

--- components/UpdateUserForm.tsx
+++ components/UpdateUserForm.tsx
@@ -48,7 +48,7 @@ export default function UpdateUserForm({formId,formData,setFormData}:any) {
                 </div>
             </div>
 
-            <button className="flex justify-center text-md w-2/6 bg-yellow-400 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">
+            <button className="flex justify-center text-md w-2/6 bg-yellow-400 text-black px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">
                 Update <span className="px-1"><BiBrush size={24}></BiBrush></span>
             </button>
 
